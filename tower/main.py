21717eb1600b7fb91b5f02fd8a0ec48aa097b736
Yisus <jesusmalcaraz@gmail.com>
2023-11-30 20:33:51 +0100

    Setup

--- tower/main.py
+++ tower/main.py
@@ -1,26 +1,34 @@
 import pygame as pg
-import constants as c
+#import constants as c
+from .constants import *
 
-#initialise pygame
-pg.init()
+def start_game():
+  #initialise pygame
+  pg.init()
 
-#create clock
-clock = pg.time.Clock()
+  #create clock
+  clock = pg.time.Clock()
 
-#create game window
-screen = pg.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
-pg.display.set_caption("Tower Defence")
+  #create game window
+  screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  pg.display.set_caption("Tower Defense")
 
-#game loop
-run = True
-while run:
+  #game loop
+  run = True
+  while run:
 
-  clock.tick(c.FPS)
+    clock.tick(FPS)
 
-  #event handler
-  for event in pg.event.get():
-    #quit program
-    if event.type == pg.QUIT:
-      run = False
+    #event handler
+    for event in pg.event.get():
+      #quit program
+      if event.type == pg.QUIT:
+        run = False
 
-pg.quit()
+  pg.quit()
+
+def main():
+  start_game()
+  
+if __name__ == "__main__":
+    main()
